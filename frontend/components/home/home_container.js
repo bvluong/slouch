7abf89358cad377e2d0a019aa27364397fa9af87
bvluong <bryantvinluong@gmail.com>
2017-05-15 16:39:37 -0700

    Add Home Container

---
+++ frontend/components/home/home_container.js
@@ -0,0 +1,10 @@
+import { connect } from "react-redux";
+import { login, signin } from "../../actions/session_actions";
+import HomeIndex from './home_index.jsx';
+
+const mapDispatchToState = dispatch => ({
+  login: user => dispatch(login(user)),
+  signin: user => dispatch(signin(user))
+});
+
+export default (null,mapDispatchToState)(HomeIndex);
