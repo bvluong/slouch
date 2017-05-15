0c0a893b969d30ffee12db0b5dfa5a991dc6374e
bvluong <bryantvinluong@gmail.com>
2017-05-15 11:02:06 -0700

    Add session actions

--- frontend/slouch.jsx
+++ frontend/slouch.jsx
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+
+  //  testing
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+
+
   ReactDOM.render(<div>Working</div>, root);
 });
+
+//testing
+import { login, signup, logout } from './util/session_api_util';
