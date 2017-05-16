e7f030759f870e835d681321b3a188f0b0b88236
Admin <admin@Bryant-Luong.local>
2017-05-16 13:26:14 -0700

    Add protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import SessionFormContainer from './session/session_form_container';
 import HomeIndexContainer from './home/home_index_container';
 import MainApp from './main/main_app';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <Switch>
-      <Route path='/main' component={MainApp}/>
-      <Route path='/' component={HomeIndexContainer}/>
+      <ProtectedRoute path='/main' component={MainApp}/>
+      <AuthRoute path='/' component={HomeIndexContainer}/>
     </Switch>
       <AuthRoute path='/login' component={SessionFormContainer}/>
       <AuthRoute path='/signup' component={SessionFormContainer}/>
--- frontend/components/home/home_nav.jsx
+++ frontend/components/home/home_nav.jsx
@@ -6,11 +6,11 @@ class HomeNav extends React.Component {
   demoHandler(e) {
     e.preventDefault();
     this.props.login({username: "demo", password:"password"})
-    .then(this.props.history.push("/main"));
+    .then(this.props.history.replace("/main"));
+    console.log(this.props);
   }
 
   render() {
-    console.log(this.props);
     return(
     <div className="home-nav" >
       <button className="home-button" onClick={this.demoHandler.bind(this)}
