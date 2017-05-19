74510cf03fb475eb13685b3e27db5073e08948e9
bvluong <bryantvinluong@gmail.com>
2017-05-18 21:47:01 -0700

    Update for heroku

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,5 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
-    const  RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
-    const receiveMessage = message => ({
-      type: RECEIVE_MESSAGE,
-      message
-    });
-    return store.dispatch(receiveMessage(data));
+    return store.dispatch({type: "RECEIVE_MESSAGE", message: data})
   },
 });
