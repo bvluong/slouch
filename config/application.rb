8d5e5c38187ffc66f894a638c17926ab59799a61
bvluong <bryantvinluong@gmail.com>
2017-05-18 21:34:02 -0700

    Implement double nested hover effect quasi selector on the retro encapulator flex

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+
 
 module Slouch
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
