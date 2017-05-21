f3f2154783b2aa04842be41fa40c6be20c407759
bvluong <bryantvinluong@gmail.com>
2017-05-20 17:55:38 -0700

    Add click user to the direct message form creation

--- frontend/components/channel_form/channel_form.jsx
+++ frontend/components/channel_form/channel_form.jsx
@@ -5,9 +5,13 @@ class ChannelForm extends React.Component {
     super(props);
     this.state = { channel_name: "",
       searchusers: [],
-      newchannel: []};
+      selected_userid: [],
+      selected_users: [],
+      render: false};
     this.updateHandler = this.updateHandler.bind(this);
     this.submitHandler = this.submitHandler.bind(this);
+    this.userHandler = this.userHandler.bind(this);
+    this.mapUsers = this.mapUsers.bind(this);
   }
 
   componentDidMount() {
@@ -16,18 +20,52 @@ class ChannelForm extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (this.props.users.length !== nextProps.users.length) {
-      const searchusers = nextProps.users.map(user =>
-        <li key={user.id}>
-          <button className="user-button" type="button">
-          <img className={`user-icon ${
-              ["red", "blue", "orange", "green", "yellow", "pink", "teal", "grey"]
-              [Math.floor(Math.random() * 8)]}`}
-             src={user.avatar}/>
-          <span>{user.username}</span>
-          </button>
-        </li>);
+      const searchusers = this.mapUsers(nextProps.users);
       this.setState({ searchusers } );
     }
+  }
+
+  componentDidUpdate() {
+    if (this.state.render) {
+      this.setState({render:false});
+      const searchusers = this.mapUsers(this.props.users);
+      this.setState({ searchusers } );
+    }
+  }
+
+  userHandler(user) {
+    return e => {
+    const selected_userid = this.state.selected_userid.concat(user.id);
+    const selected_users = this.state.selected_users.concat(user);
+    this.setState({
+      channel_name: "",
+      selected_userid,
+      selected_users,
+      render: true
+    });
+  };
+  }
+
+  removeHandler(user) {
+    return e => {
+      const userid_index = this.state.selected_userid.indexOf(user.id);
+      const user_index = this.state.selected_users.indexOf(user);
+      
+    };
+  }
+
+  mapUsers(users) {
+    const filter_users = users.filter(user => !this.state.selected_userid.includes(user.id) );
+    return filter_users.map(user =>
+      <li key={user.id}>
+        <button className="user-button" type="button" onClick={this.userHandler(user)}>
+        <img className={`user-icon ${
+            ["red", "blue", "orange", "green", "yellow", "pink", "teal", "grey"]
+            [Math.floor(Math.random() * 8)]}`}
+           src={user.avatar}/>
+        <span>{user.username}</span>
+        </button>
+      </li>);
   }
 
 
@@ -35,16 +73,7 @@ class ChannelForm extends React.Component {
     return (e) => {
       this.setState({ [field]: e.target.value });
       const filter_users = this.props.users.filter(user => user.username.toLowerCase().includes(e.target.value) );
-      const searchusers = filter_users.map(user =>
-        <li key={user.id}>
-          <button className="user-button" type="button">
-          <img className={`user-icon ${
-              ["red", "blue", "orange", "green", "yellow", "pink", "teal", "grey"]
-              [Math.floor(Math.random() * 8)]}`}
-              src={user.avatar}/>
-          <span>{user.username}</span>
-          </button>
-        </li>);
+      const searchusers = this.mapUsers(filter_users);
       this.setState({ searchusers } );
     };
   }
@@ -57,6 +86,7 @@ class ChannelForm extends React.Component {
 
   render() {
     const { users } = this.props;
+    console.log(this.state);
     return (
       <div className="channel-form-page">
         <div className="channel-form">
@@ -70,6 +100,16 @@ class ChannelForm extends React.Component {
               <input className="channel-button" type="submit" value="Go"/>
             </form>
           </div>
+
+          <ul className="new-channel">
+          { this.state.selected_users.map(user => <li key={user.username} onClick={this.removeHandler(user).bind(this)}>
+              <img className={`user-icon ${
+                  ["red", "blue", "orange", "green", "yellow", "pink", "teal", "grey"]
+                  [Math.floor(Math.random() * 8)]}`}
+                 src={user.avatar}/>
+              <span>{user.username}</span>
+            </li> )}
+          </ul>
 
           <div className="user-names">
             <ul className="user-names-details">
