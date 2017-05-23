4b392d4d6f55ea675c44ea5a902c849c83ef748f
bvluong <bryantvinluong@gmail.com>
2017-05-23 10:15:20 -0700

    Add reactions controller

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -2,6 +2,11 @@ class ReactionsController < ApplicationController
   def create
     @reaction = current_user.reactions.new(reactions_params)
     if @reaction.save
+      @message = @reaction.message
+      render "api/messages/#{@message.id}"
+    else
+      render json: @reaction.errors.full_messages, status: 422
+    end
   end
 
   def destroy
