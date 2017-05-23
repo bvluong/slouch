a04ec4d4e128f7248574578631124d49e2f0f232
bvluong <bryantvinluong@gmail.com>
2017-05-22 22:00:55 -0700

    Add emojis to the sumbit message

--- frontend/components/main_chat/main_chat.jsx
+++ frontend/components/main_chat/main_chat.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MessageIndexContainer from './message_index_container';
 import { merge } from 'lodash';
 import ReactEmoji from 'react-emoji';
+import emojis from 'emojis-list';
 
 class MainChat extends React.Component {
   constructor(props) {
@@ -33,31 +34,28 @@ class MainChat extends React.Component {
     this.setState({showEmojis: !this.state.showEmojis});
   }
 
-  addEmoji(emoji) {
+  addEmoji(emo) {
     return e => {
     e.preventDefault();
-    this.setState({body: (this.state.body+emoji) });
+    this.setState({body: (this.state.body+emo) });
   };
   }
 
   emojiChoices() {
+    console.log(emojis[0]);
     return (
-    <ul className="emoji-box"> {
-    [':smirk:',':heart_eyes:',':kissing_heart:',
-    ':flushed:',':relieved:',
-    ':grin:',':wink:',
-    ':stuck_out_tongue_closed_eyes:',':grinning:',
-    ':kissing_smiling_eyes:',':stuck_out_tongue:',
-    ':worried:',':frowning:',
-    ':open_mouth:',':grimacing:'].map( (emoji,idx) => <li key={idx} onClick={this.addEmoji(emoji)}> {ReactEmoji.emojify(emoji)} </li>)
-    } </ul>
+    <ul className="emoji-box"> <h4>Choose an emoji</h4>
+    {emojis.slice(1634,2000)
+    .map( (emo,idx) => <li key={idx}
+      onClick={this.addEmoji(emo)}>{emo}</li>)}
+    </ul>
   );
   }
 
 
   render () {
     const { currentChannel } = this.props;
-    const emojis = this.state.showEmojis ? this.emojiChoices() : "" ;
+    const emojilist = this.state.showEmojis ? this.emojiChoices() : "" ;
     return (
       <div className="main-chat">
         <MessageIndexContainer currentChannel={currentChannel}/>
@@ -73,7 +71,7 @@ class MainChat extends React.Component {
             <button type="button" className="emoji-button" onClick={this.showEmojis}>
                 <i className="fa fa-smile-o" id="emoji-icon" aria-hidden="true"></i>
               </button>
-              {emojis}
+              {emojilist}
          </div>
       </div>
     );
@@ -81,3 +79,11 @@ class MainChat extends React.Component {
 }
 
 export default MainChat;
+
+// [`:blush:`,`:smiley:`,`:relaxed:`,`:smirk:`,`:heart_eyes:`,`:kissing_heart:`,`:kissing_closed_eyes:`,`:flushed:`,`:relieved:`,`:satisfied:`,`:grin:`,`:wink:`,`:stuck_out_tongue_winking_eye:`,`:stuck_out_tongue_closed_eyes:`,`:grinning:`,`:kissing:`,`:kissing_smiling_eyes:`,`:stuck_out_tongue:`,`:sleeping:`,`:worried:`,`:frowning:`,`:anguished:`,`:open_mouth:`,`:grimacing:`,`:confused:`,
+// `:hushed:`,`:expressionless:`,`:unamused:`,`:sweat_smile:`,`:sweat:`,`:disappointed_relieved:`,`:weary:`,`:pensive:`,`:disappointed:`,`:confounded:`,`:fearful:`,`:cold_sweat:`,`:persevere:`,`:cry:`,`:sob:`,`:joy:`,`:astonished:`,`:scream:`,`:tired_face:`,`:angry:`,`:rage:`,`:triumph:`,`:sleepy:`,`:yum:`,`:mask:`,`:sunglasses:`,`:dizzy_face:`,`:imp:`,`:smiling_imp:`,
+// `:neutral_face:`,`:no_mouth:`,`:innocent:`,`:alien:`,`:yellow_heart:`,`:blue_heart:`,`:purple_heart:`,`:heart:`,`:green_heart:`,`:broken_heart:`,`:heartbeat:`,`:heartpulse:`,`:two_hearts:`,`:revolving_hearts:`,`:cupid:`,`:sparkling_heart:`,`:sparkles:`,`:star:`,`:star2:`,`:dizzy:`,`:boom:`,`:collision:`,`:anger:`,`:exclamation:`,`:question:`,`:grey_exclamation:`,`:grey_question:`,
+// `:zzz:`,`:dash:`,`:sweat_drops:`,`:notes:`,`:musical_note:`,`:fire:`,`:hankey:`,`:poop:`,`:shit:`,`:+1:`,`:thumbsup:`,`:-1:`,`:thumbsdown:`,`:ok_hand:`,`:punch:`,`:facepunch:`,`:fist:`,`:v:`,`:wave:`,`:hand:`,`:raised_hand:`,`:open_hands:`,`:point_up:`,`:point_down:`,`:point_left:`,`:point_right:`,`:raised_hands:`,`:pray:`,`:point_up_2:`,`:clap:`,`:muscle:`,`:fu:`,
+// `:walking:`,`:runner:`,`:running:`,`:couple:`,`:family:`,`:two_men_holding_hands:`,`:two_women_holding_hands:`,`:dancer:`,`:dancers:`,`:ok_woman:`,`:no_good:`,`:information_desk_person:`,`:raising_hand:`,`:bride_with_veil:`,`:person_with_pouting_face:`,`:person_frowning:`,`:bow:`,`:couplekiss:`,`:couple_with_heart:`,`:massage:`,`:haircut:`,`:nail_care:`,`:boy:`,`:girl:`,
+// `:woman:`,`:man:`,`:baby:`,`:older_woman:`,`:older_man:`,`:person_with_blond_hair:`,`:man_with_gua_pi_mao:`,`:man_with_turban:`,`:construction_worker:`,`:cop:`,`:angel:`,`:princess:`,`:smiley_cat:`,`:smile_cat:`,`:heart_eyes_cat:`,`:kissing_cat:`,`:smirk_cat:`,`:scream_cat:`,`:crying_cat_face:`,`:joy_cat:`,`:pouting_cat:`,`:japanese_ogre:`,`:japanese_goblin:`,`:see_no_evil:`,
+// `:hear_no_evil:`,`:speak_no_evil:`,`:guardsman:`,`:skull:`,`:feet:`,`:lips:`,`:kiss:`,`:droplet:`,`:ear:`,`:eyes:`,`:nose:`,`:tongue:`,`:love_letter:`,`:bust_in_silhouette:`,`:busts_in_silhouette:`,`:speech_balloon:`,`:thought_balloon:`]
