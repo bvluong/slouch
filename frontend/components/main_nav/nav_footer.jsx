af1b88de4189aaa7a30fd293b0c65a53d880decc
bvluong <bryantvinluong@gmail.com>
2017-05-25 22:28:11 -0700

    Add a features button to footer

--- frontend/components/main_nav/nav_footer.jsx
+++ frontend/components/main_nav/nav_footer.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
-const NavChannel = () => (
-  <div className="nav-footer">
-    <a href="https://github.com/bvluong" target="_blank">
-      <i className="fa fa-github" id="fa-contact" aria-hidden="true"></i>
-    </a>
-    <a href="https://www.linkedin.com/in/bryant-luong/" target="_blank">
-      <i className="fa fa-linkedin" id="fa-contact" aria-hidden="true"></i>
-    </a>
-    <a href="mailto:bryantvinluong@gmail.com?Subject=Hello" target="_blank">
-      <i className="fa fa-envelope-o" id="fa-contact" aria-hidden="true"></i>
-    </a>
 
-  </div>
-);
+class NavChannel extends React.Component {
+
+  render() {
+    return (
+      <div className="nav-bottom" >
+        <button type='button'>Features</button>
+        <div className="nav-footer">
+          <a href="https://github.com/bvluong" target="_blank">
+            <i className="fa fa-github" id="fa-contact" aria-hidden="true"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/bryant-luong/" target="_blank">
+            <i className="fa fa-linkedin" id="fa-contact" aria-hidden="true"></i>
+          </a>
+          <a href="mailto:bryantvinluong@gmail.com?Subject=Hello" target="_blank">
+            <i className="fa fa-envelope-o" id="fa-contact" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default NavChannel;
