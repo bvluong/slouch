17a80b7361cdcc196851fd1fa5fcc762fb1fbfb1
bvluong <bryantvinluong@gmail.com>
2017-05-25 23:03:06 -0700

    Add modal to feature button

---
+++ frontend/components/main_nav/features.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Features = () => (
+  <div className="features-home">
+    <div className="features-notifications">
+      Notification
+      
+    </div>
+    <div className="features-messages">
+      Messages
+
+    </div>
+    <div className="features-search">
+      Feature Search
+
+    </div>
+  </div>
+);
+
+export default Features;
--- frontend/components/main_nav/nav_footer.jsx
+++ frontend/components/main_nav/nav_footer.jsx
@@ -1,12 +1,74 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import Features from './features';
 
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '60%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    transform             : 'translate(-50%, -50%)',
+    height                : '90%',
+    width                 : '60%',
+    overflow              : 'none'
+  }
+};
 
 class NavChannel extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { modalIsOpen: false };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    const appElement = document.getElementById('root');
+    Modal.setAppElement(appElement);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  toggleModal() {
+    this.setState({modalIsOpen: !this.state.modalIsOpen});
+  }
 
   render() {
     return (
       <div className="nav-bottom" >
-        <button type='button'>Features</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal">
+
+            <div className="modal-form-close">
+              <button className="close-button" onClick={this.closeModal}>
+                <i className="fa fa-times-circle fa-2x"
+                  aria-hidden="true"
+                  id="fa-icons"/>
+              </button>
+            </div>
+
+            <Features closeModal={this.closeModal.bind(this)}/>
+
+          </Modal>
+
+          <button className="modal-button" onClick={this.toggleModal}>
+            Features
+          </button>
+
         <div className="nav-footer">
           <a href="https://github.com/bvluong" target="_blank">
             <i className="fa fa-github" id="fa-contact" aria-hidden="true"></i>
